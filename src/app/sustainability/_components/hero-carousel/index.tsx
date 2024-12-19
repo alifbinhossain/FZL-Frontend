56f68d0407ce52904fb7f563368fb167308be901
alifbinhossain <alifahmed47@gmail.com>
2024-12-19 12:19:50 +0600

    ci: :sparkles: completed sustainability page

---
+++ src/app/sustainability/_components/hero-carousel/index.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import React from 'react';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+
+import sliders from '../../_const/sliders';
+
+const HeroCarousel = () => {
+  return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      {sliders.map((slide, index) => (
+        <SwiperSlide key={index}>
+          <div
+            style={{
+              backgroundImage: `url(${slide.image})`,
+            }}
+            className='h-[90vh] w-full relative bg-cover bg-center'
+          >
+            <div className='absolute inset-0 bg-black/10'></div>
+            <div className='absolute inset-0 z-10'>{slide.content}</div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default HeroCarousel;
--- src/types/index.ts
+++ src/types/index.ts
@@ -26,3 +26,8 @@ export type ICertificate = {
   title: string;
   image: string;
 };
+
+export type ISlide = {
+  content: React.ReactNode;
+  image: string;
+};
